4c7afe7eccd12a863876439dc68a965a1f903200
James Cool <jeffreysbrother@gmail.com>
2016-07-05 22:25:41 -0700

    testing with postman

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 var express = require("express");
 var app = express();
 
+app.use(function(req, res, next){
+  console.log("the leaves on the trees are", req.query.color);
+  next();
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
